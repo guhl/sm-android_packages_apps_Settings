07892485c2dfeada0056fe3d6bb339f052c74902
Chris Soyars <ctso@ctso.me>
2010-12-19 17:26:58 -0500

    Add Modver to Settings.
    Change-Id: I596064a80210517ff0dae30401f71b52adabd614
    
    Rename modversion
    
    Change-Id: I6557d5aa68a3ad00166cdd2b8951f425ad7800df

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -70,6 +70,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
     private static final String KEY_MOD_BUILD_DATE = "build_date";
     private static final String KEY_DEVICE_CPU = "device_cpu";
     private static final String KEY_DEVICE_MEMORY = "device_memory";
+    private static final String KEY_MOD_VERSION = "mod_version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -93,6 +94,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
+        setValueSummary(KEY_MOD_VERSION, "ro.sm.version");
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
