784fe37c068c8d599979ac1bada92153c49afdc1
Paul Beeler <sparksco@gmail.com>
2013-03-25 04:51:31 -0600

    Settings: Power menu options Signed-off-by: Paul Beeler <sparksco@gmail.com>

--- src/com/android/settings/saber/PowerMenu.java
+++ src/com/android/settings/saber/PowerMenu.java
@@ -29,8 +29,16 @@ public class PowerMenu extends SettingsPreferenceFragment {
     private static final String TAG = "PowerMenu";
 
     private static final String KEY_EXPANDED_DESKTOP = "power_menu_expanded_desktop";
+    private static final String KEY_REBOOT = "power_menu_reboot";
+    private static final String KEY_SCREENSHOT = "power_menu_screenshot";
+    private static final String KEY_AIRPLANE = "power_menu_airplane";
+    private static final String KEY_SILENT = "power_menu_silent";
 
     private CheckBoxPreference mExpandedDesktopPref;
+    private CheckBoxPreference mRebootPref;
+    private CheckBoxPreference mScreenshotPref;
+    private CheckBoxPreference mAirplanePref;
+    private CheckBoxPreference mSilentPref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,6 +57,22 @@ public class PowerMenu extends SettingsPreferenceFragment {
             mExpandedDesktopPref.setChecked((Settings.System.getInt(getContentResolver(),
                 Settings.System.POWER_MENU_EXPANDED_DESKTOP_ENABLED, 0) == 1));
         }
+
+        mRebootPref = (CheckBoxPreference) findPreference(KEY_REBOOT);
+        mRebootPref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWER_MENU_REBOOT_ENABLED, 1) == 1));
+
+        mScreenshotPref = (CheckBoxPreference) findPreference(KEY_SCREENSHOT);
+        mScreenshotPref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWER_MENU_SCREENSHOT_ENABLED, 0) == 1));
+
+        mAirplanePref = (CheckBoxPreference) findPreference(KEY_AIRPLANE);
+        mAirplanePref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWER_MENU_AIRPLANE_ENABLED, 1) == 1));
+
+        mSilentPref = (CheckBoxPreference) findPreference(KEY_SILENT);
+        mSilentPref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWER_MENU_SILENT_ENABLED, 1) == 1));
     }
 
     @Override
@@ -59,6 +83,26 @@ public class PowerMenu extends SettingsPreferenceFragment {
             value = mExpandedDesktopPref.isChecked();
             Settings.System.putInt(getContentResolver(),
                     Settings.System.POWER_MENU_EXPANDED_DESKTOP_ENABLED,
+                    value ? 1 : 0);
+        } else if (preference == mScreenshotPref) {
+            value = mScreenshotPref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.POWER_MENU_SCREENSHOT_ENABLED,
+                    value ? 1 : 0);
+        } else if (preference == mRebootPref) {
+            value = mRebootPref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.POWER_MENU_REBOOT_ENABLED,
+                    value ? 1 : 0);
+        } else if (preference == mAirplanePref) {
+            value = mAirplanePref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.POWER_MENU_AIRPLANE_ENABLED,
+                    value ? 1 : 0);
+        } else if (preference == mSilentPref) {
+            value = mSilentPref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.POWER_MENU_SILENT_ENABLED,
                     value ? 1 : 0);
         } else {
             return super.onPreferenceTreeClick(preferenceScreen, preference);
